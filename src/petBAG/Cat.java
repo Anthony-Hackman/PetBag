d17686627183224e4b3bb7c65eb64e37a4eedf43
Anthony Hackman <143662667+Anthony-Hackman@users.noreply.github.com>
2023-09-11 23:25:05 -0700

    Add files via upload
    Organization

---
+++ src/petBAG/Cat.java
@@ -0,0 +1,19 @@
+package petBAG;
+
+public class Cat {
+	private int catSpaceNumber;
+	public Cat()
+	{
+		catSpaceNumber = -1;
+	}
+	
+	//access and mutators
+	public void setCatSpaceNumber(int cs)
+	{
+		catSpaceNumber = cs;
+	}
+	public int getCatSpaceNumber()
+	{
+		return catSpaceNumber;
+	}
+}
